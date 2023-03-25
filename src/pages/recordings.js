f04a1aa70e27d408e252b02e1eac43ef379d7c7e
GoViper <nithishbanda2021@gmail.com>
2023-03-26 01:41:09 +0530

    recordBug

--- src/pages/recordings.js
+++ src/pages/recordings.js
@@ -25,38 +25,7 @@ export default function Recordings(props) {
   const set = "/Recordings/" + video
   return (
     <>
-      <div style={{margin: '20px'}}>
-        <h1>Recording for {topic}</h1>
-          <h2>ID : {id} </h2>
-        <Divider />
-        <br/>
-        <div style={{display: 'flex',  alignItems: 'center'}}>
-          { btn === 'Transcript' ?
-            <video width='55%' height='40%' controls>
-                <source src={set} type="video/mp4" />
-            </video> : 
-            <Card shadow="sm" padding="xs"  radius="md"   withBorder style={{height: '400px', cursor: 'pointer', width: '60%' }}>
-              <h2 style={{textAlign: 'center'}}>Transcript</h2>
-              {console.log('hello')}
-              <h3>{trans}</h3>
-            </Card>
-          }
-          <Card shadow="sm" padding="xs"  radius="md"   withBorder style={{height: '400px', cursor: 'pointer', marginLeft: '30px', width: '30%' }}>
-            <h2 style={{textAlign: 'center'}}>Chat Window</h2>
-            
-          </Card>
-        </div>
-        <br/>
-        <Group>
-          <Button size="md" onClick={() => router.push('/')}>
-            Go Back
-          </Button>
-          <Button size="md" variant="outline" onClick={toggleHandler}>
-            Toggle {btn}
-          </Button>
-        </Group>
-        
-      </div>
+     
     </>
   );
 }
